003f527b221d7b7dbeb26739655871a06c555f93
Sam Hoffman <samdhoffman@gmail.com>
2020-03-19 15:42:30 -0400

    Add layout and use in app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.scss';
 import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
+import Layout from './components/layout/Layout';
 
 function App() {
   const [images, setImages] = useState([]);
@@ -27,20 +28,22 @@ function App() {
   
   return (
     <div className="App">
-      {isError && <div>Something went wrong ...</div>}
+      <Layout>
+        {isError && <div>Something went wrong ...</div>}
 
-      {isLoading ? (
-        <div>Loading ...</div>
-      ) : (
-        <Grid container spacing={2} alignItems="center">
-          {images.map(img => (
-            // auto-layout being used for Grid item to make items equitably share available space
-            <Grid item xs key={img.url}> 
-              <img src={img.url}/>
-            </Grid>
-          ))}
-        </Grid>
-      )}
+        {isLoading ? (
+          <div>Loading ...</div>
+        ) : (
+          <Grid container spacing={2} alignItems="center">
+            {images.map(img => (
+              // auto-layout being used for Grid item to make items equitably share available space
+              <Grid item xs key={img.url}> 
+                <img src={img.url} alt=""/>
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </Layout>
     </div>
   );
 }
---
+++ src/components/layout/Layout.js
@@ -0,0 +1,36 @@
+import React from "react"
+import "./Layout.scss";
+import { makeStyles } from '@material-ui/core/styles';
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+const useStyles = makeStyles({
+  Layout: {
+    display: "flex",
+    flexDirection: "column",
+    height: "100vh",
+    width: "100vw",
+
+    '& .content': {
+      display: "flex",
+      flex: "1 0 auto",
+      flexDirection: "column",
+    }
+  }
+})
+
+const Layout = (props) => {
+  const classes = useStyles();
+  
+  return (
+    <div className={classes.Layout}>
+      <Navbar />
+      <main className="content">
+        {props.children}
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout;
