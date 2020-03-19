34030f3a3ef77ea197758f6a1bd26685a515aa95
Sam Hoffman <samdhoffman@gmail.com>
2020-03-19 18:36:39 -0400

    Add initial pagination implementation

--- src/App.js
+++ src/App.js
@@ -2,29 +2,41 @@ import React, { useState, useEffect } from 'react';
 import './App.scss';
 import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
+import Pagination from '@material-ui/lab/Pagination';
 import Layout from './components/layout/Layout';
 
 function App() {
   const [images, setImages] = useState([]);
+  const [pages, setPages] = useState(0);
+  const [curPage, setCurPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false); // used for loading indicator
   const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
-
-      try {
-        const result = await axios.get('/image'); // get images data from our api
-        setImages(result.data);
-      } catch (error) {
-        setIsError(true);
-      }
-
-      setIsLoading(false);
-    };
-    fetchData();
+    fetchData(curPage);
   }, []);
+
+  // When user changes page this will set a new current page and make a GET request for the next page of data
+  const handlePageChange = (event, value) => {
+    setCurPage(value);
+    fetchData(value);
+  }
+
+  // Load our image data here using page as a param for pagination
+  const fetchData = async (page) => {
+    setIsError(false);
+    setIsLoading(true);
+
+    try {
+      const result = await axios.get(`/image?page=${page}`); // get images data from our api
+      setImages(result.data.images);
+      setPages(result.data.pages);
+    } catch (error) {
+      setIsError(true);
+    }
+
+    setIsLoading(false);
+  };
   
   return (
     <div className="App">
@@ -43,6 +55,8 @@ function App() {
             ))}
           </Grid>
         )}
+
+        <Pagination count={pages} page={curPage} onChange={handlePageChange} />
       </Layout>
     </div>
   );
