ee744baf7b3586fdc03f0a456787d33d01b1b316
Sam Hoffman <samdhoffman@gmail.com>
2020-03-20 12:28:55 -0400

    Add GrayscaleToggle component

---
+++ src/components/GrayscaleToggle.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+import InvertColorsIcon from '@material-ui/icons/InvertColors';
+
+export default function GrayscaleToggle({ toggleGrayscale }) {
+  return (
+    <div>
+      {/* Toggle Images to Grayscale */}
+      <Button
+        variant="contained"
+        color="default"
+        className="grayscale-btn"
+        startIcon={<InvertColorsIcon />}
+        onClick={toggleGrayscale}
+      >
+        Toggle Grayscale
+      </Button>
+    </div>
+  )
+}
