f3568c0b5c2ee90d878636304a444f2a0e2ce990
Sam Hoffman <samdhoffman@gmail.com>
2020-03-21 10:42:26 -0400

    Add loading spinner

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.scss';
 import { makeStyles } from '@material-ui/core/styles';
 import axios from 'axios';
 
+import Grid from '@material-ui/core/Grid';
 import Pagination from '@material-ui/lab/Pagination';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import Layout from './components/layout/Layout';
 import ControlPanel from './components/ControlPanel';
@@ -38,7 +40,7 @@ function App() {
 
   useEffect(() => {
     fetchData(curPage); // Get images on page load
-  }, [curPage]);
+  }, [curPage]); // Having curPage in the dependency array will ensure that fetchData is run every time the page changes
 
   // When user changes page this will set a new current page and make a GET request for the next page of data
   const handlePageChange = (event, value) => {
@@ -50,15 +52,18 @@ function App() {
     setIsError(false);
     setIsLoading(true);
 
+    
     try {
       const result = await axios.get(`/images?page=${page}`); // get images data from our api
-      setImages(result.data.images);
-      setPages(result.data.pages);
+      setTimeout(() => {
+        setImages(result.data.images);
+        setPages(result.data.pages);
+        setIsLoading(false);
+      }, 1000)
     } catch (error) {
       setIsError(true);
+      setIsLoading(false)
     }
-
-    setIsLoading(false);
   };
 
   // Load our image data here using page as a param for pagination
@@ -68,13 +73,15 @@ function App() {
 
     try {
       const result = await axios.get(`/images/filter?width=${widthOpt}&height=${heightOpt}`); // pass width andheight as query params
-      setImages(result.data.images);
-      setPages(result.data.pages);
+      setTimeout(() => {
+        setImages(result.data.images);
+        setPages(result.data.pages);
+        setIsLoading(false);
+      }, 1000)
     } catch (error) {
       setIsError(true);
+      setIsLoading(false);
     }
-
-    setIsLoading(false);
   };
 
   const toggleGrayscale = () => {
@@ -98,7 +105,9 @@ function App() {
         {isError && <div>Something went wrong ...</div>}
 
         {isLoading ? (
-          <div>Loading ...</div>
+          <Grid container justify="center">
+            <CircularProgress />
+          </Grid>
         ) : (
           <ImageGrid images={images} />
         )}
