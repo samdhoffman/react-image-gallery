4778bcef37ccadb9a5ca81824dfc59950fb9f0a8
Sam Hoffman <samdhoffman@gmail.com>
2020-03-22 14:50:17 -0400

    Add info icon to open info modal on image grid

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Grid from '@material-ui/core/Grid';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
-import { useTheme } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import InfoIcon from '@material-ui/icons/Info';
 import ImageModal from './ImageModal';
 
 const useStyles = makeStyles(theme => ({
@@ -16,6 +19,19 @@ const useStyles = makeStyles(theme => ({
   gridList: {
     width: "80%",
     height: "80%",
+  },
+  tileBar: {
+    background: "transparent"
+  },
+  imgContainer: {
+    position: "relative",
+  },
+  infoIcon: {
+    color: 'rgba(255, 255, 255, 0.54)',
+    position: "absolute",
+    bottom: 10,
+    right: 5,
+    padding: 0,
   },
 }));
 
@@ -40,7 +56,12 @@ export default function ImageGrid({ images }) {
           {images.map(img => (
             // auto-layout being used for Grid item to make items equitably share available space
             <Grid container item xs key={img.url} justify="center"> 
-              <img src={img.url} alt="" onClick={() => handleModalOpen(img)} />
+              <div className={classes.imgContainer} >
+                <img src={img.url} alt=""/>
+                <IconButton aria-label={`info about ${img.url}`} className={classes.infoIcon} onClick={() => handleModalOpen(img)}>
+                  <InfoIcon />
+                </IconButton>
+              </div>
             </Grid>
           ))}
         </Grid>
@@ -48,7 +69,15 @@ export default function ImageGrid({ images }) {
         <GridList cellHeight={100} className={classes.gridList} cols={12}>
           {images.map(img => (
             <GridListTile key={img.url} cols={Math.ceil(img.width/100)} rows={Math.ceil(img.height/100)}>
-              <img src={img.url} alt="" onClick={() => handleModalOpen(img)} />
+              <img src={img.url} alt=""/>
+              <GridListTileBar
+                className={classes.tileBar}
+                actionIcon={
+                  <IconButton aria-label={`info about ${img.url}`} className={classes.infoIcon} onClick={() => handleModalOpen(img)}>
+                    <InfoIcon />
+                  </IconButton>
+                }
+              />
             </GridListTile>
           ))}
         </GridList>
