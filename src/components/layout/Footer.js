3778cc538bd9c59fae1b3fa4aa69955aa8a4e75c
Sam Hoffman <samdhoffman@gmail.com>
2020-03-22 18:56:51 -0400

    Update footer with github and linkedin links

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,20 +1,53 @@
 import React from "react"
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   Footer: {
     textAlign: "center",
-    flexShrink: 0
+    flexShrink: 0,
+    background: "rgba(0, 0, 0, 1)",
+    color: "white",
+  },
+  author: {
+    ...theme.typography.subtitle1,
+    display: "flex",
+    justifyContent: "center",
+  },
+  footerBtn: {
+    border: "none",
+  },
+  footerLink: {
+    '&:visited': {
+      color: 'inherit',
+    }
   }
-})
+}));
 
 const Footer = () => {
   const classes = useStyles();
 
   return (
     <footer className={classes.Footer}>
-      <p>by Sam Hoffman</p>
-      <p>For your consideration</p>
+      <section className={classes.author}>
+        <p>Sam Hoffman</p>
+      </section>
+      
+      <ButtonGroup color="inherit" aria-label="button group">
+        <Button className={classes.footerBtn}>
+          <a className={classes.footerLink} href="https://github.com/samdhoffman" target="_blank" rel="noopener noreferrer" >
+            <GitHubIcon/>
+          </a>
+        </Button>
+        <Button className={classes.footerBtn}>
+          <a className={classes.footerLink} href="https://www.linkedin.com/in/samdhoffman/" target="_blank" rel="noopener noreferrer" >
+            <LinkedInIcon/>
+          </a>
+        </Button>
+      </ButtonGroup>
     </footer>
   )
 }
