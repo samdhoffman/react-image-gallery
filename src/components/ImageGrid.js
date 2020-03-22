aae148d60ec59271537ba3756634ab0bc48d553d
Sam Hoffman <samdhoffman@gmail.com>
2020-03-22 14:55:27 -0400

    Remove GridList disaply option from ImageGrid

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,11 +1,6 @@
 import React, { useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
-import { useTheme } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Grid from '@material-ui/core/Grid';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
 import ImageModal from './ImageModal';
@@ -15,13 +10,6 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     justifyContent: 'space-around',
     overflow: 'hidden',
-  },
-  gridList: {
-    width: "80%",
-    height: "80%",
-  },
-  tileBar: {
-    background: "transparent"
   },
   imgContainer: {
     position: "relative",
@@ -37,8 +25,6 @@ const useStyles = makeStyles(theme => ({
 
 export default function ImageGrid({ images }) {
   const classes = useStyles();
-  const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.down('sm'));
 
   const [open, setOpen] = useState(false);
   const [curImage, setCurImage] = useState({});
@@ -50,8 +36,6 @@ export default function ImageGrid({ images }) {
 
   return (
     <div className={classes.root}>
-      {
-        matches ?
         <Grid container spacing={2} alignItems="center">
           {images.map(img => (
             // auto-layout being used for Grid item to make items equitably share available space
@@ -65,23 +49,6 @@ export default function ImageGrid({ images }) {
             </Grid>
           ))}
         </Grid>
-        :
-        <GridList cellHeight={100} className={classes.gridList} cols={12}>
-          {images.map(img => (
-            <GridListTile key={img.url} cols={Math.ceil(img.width/100)} rows={Math.ceil(img.height/100)}>
-              <img src={img.url} alt=""/>
-              <GridListTileBar
-                className={classes.tileBar}
-                actionIcon={
-                  <IconButton aria-label={`info about ${img.url}`} className={classes.infoIcon} onClick={() => handleModalOpen(img)}>
-                    <InfoIcon />
-                  </IconButton>
-                }
-              />
-            </GridListTile>
-          ))}
-        </GridList>
-      }
       
       <ImageModal open={open} image={curImage} setOpen={setOpen} />
     </div>
