b98251138503377cbdfe09088664959f515b9bfe
Sam Hoffman <samdhoffman@gmail.com>
2020-03-23 10:01:35 -0400

    Add page to display if there are no image results

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -4,6 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
 import ImageModal from './ImageModal';
+import NoResults from './NoResults';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -36,6 +37,11 @@ export default function ImageGrid({ images }) {
 
   return (
     <div className={classes.root}>
+      {
+        images.length === 0
+        ?
+        <NoResults />
+        :
         <Grid container spacing={2} alignItems="center">
           {images.map(img => (
             // auto-layout being used for Grid item to make items equitably share available space
@@ -49,6 +55,7 @@ export default function ImageGrid({ images }) {
             </Grid>
           ))}
         </Grid>
+      }
       
       <ImageModal open={open} image={curImage} setOpen={setOpen} />
     </div>
---
+++ src/components/NoResults.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import doctor_reading from '../static/doctor_reading.jpg';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    height: "100%",
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-evenly"
+  },
+  drReadingImg: {
+    height: 400,
+    width: 500
+  },
+  h5: {
+    display: "inline"
+  },
+  homeBtn: {
+    margin: "0 5px"
+  }
+}));
+
+export default function NoResults() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <section className="photoContainer">
+        <img className={classes.drReadingImg} src={doctor_reading} />
+      </section>
+      <section>
+        <h2>Your Results Came Back Negative</h2>
+        <h5 className={classes.h5}>Try another search or return</h5>
+        <span className={classes.homeBtn}>
+          <Button variant="contained" href="/">Home</Button>
+        </span>
+      </section>
+    </div>
+  )
+}
