d785ef4abbae3daa571f94fac0cf4dd5fc527e3a
Sam Hoffman <samdhoffman@gmail.com>
2020-03-23 10:28:25 -0400

    Don't display NoResults page when loading

--- src/App.js
+++ src/App.js
@@ -104,7 +104,7 @@ function App() {
 
         {isError && <ErrorPage errorResponse={errorResponse} />}
         
-        {(!isError && images.length === 0) && <NoResults />}
+        {(!isLoading && !isError && images.length === 0) && <NoResults />}
 
         {isLoading ? (
           <Grid 
